630030dd014b733031a27d9a71fb2ed1a014bfb1
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2024-06-20 13:26:48 -0300

    remover TODOs concluidos

--- particles.h
+++ particles.h
@@ -20,12 +20,4 @@ void updatePredVelocity(particle *p, double alpha, double *swarm_opt);
 void updatePreyPosition(particle *p, double (*cost)(double *));
 void updatePredPosition(particle *p);
 
-// TODO: adaptar updateVelocity para updatePreyVelocity, recebendo também o
-// parâmetro λ. v_i(t+1) já está sendo calculado, só falta somar com o termo ->
-// A * exp(−λ||x_prey(t) − x_pred(t+1)||).
-// A é a influencia dos preds (distancia media)
-
-// TODO: adaptar updateVelocity para updatePredVelocity, recebendo também os
-// parâmetros α e x_opt (das presas). v_pred(t + 1) = α[x_opt(t) − x_pred(t)]
-
 #endif
